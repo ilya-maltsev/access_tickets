a37e3f5b155e2eb97cfc8b11580d444a18284881
ILYA MALTSEV <i.y.maltsev@yandex.ru>
2016-11-24 16:33:00 +0300

    Delete 1005_create_i_resgranters.rb

--- db/migrate/1005_create_i_resgranters.rb
+++
@@ -1,9 +0,0 @@
-class CreateIResgranters < ActiveRecord::Migration
-  def change
-    create_table :i_resgranters do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :i_resource, index: true
-      t.timestamps
-    end
-  end
-end
