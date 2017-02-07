2da76cc9fde66855eb9735469b1bae7ea483311b
ILYA MALTSEV <i.y.maltsev@yandex.ru>
2017-02-07 16:40:32 +0400

    Update 4007_create_i_resources.rb

--- db/migrate/4007_create_i_resources.rb
+++ db/migrate/4007_create_i_resources.rb
@@ -1,5 +1,5 @@
 class CreateIResources < ActiveRecord::Migration
-  def change
+  def up
     create_table :i_resources do |t|
       t.string :name
       t.string :description, :null => true
@@ -9,5 +9,9 @@ class CreateIResources < ActiveRecord::Migration
       t.belongs_to :updated_by, :null => true
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :i_resources
   end
 end
