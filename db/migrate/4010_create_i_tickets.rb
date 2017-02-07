33accc56f17bd701f72964ae538b33c0d5580d22
ILYA MALTSEV <i.y.maltsev@yandex.ru>
2017-02-07 16:41:43 +0400

    Update 4010_create_i_tickets.rb

--- db/migrate/4010_create_i_tickets.rb
+++ db/migrate/4010_create_i_tickets.rb
@@ -1,5 +1,5 @@
 class CreateITickets < ActiveRecord::Migration
-  def change
+  def up
     create_table :i_tickets do |t|
       t.belongs_to :issue, index: true, :null => true
       t.belongs_to :user, index: true, :null => true
@@ -20,5 +20,9 @@ class CreateITickets < ActiveRecord::Migration
       t.belongs_to :created_by, :null => true
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :i_tickets
   end
 end
