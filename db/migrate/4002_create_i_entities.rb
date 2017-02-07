87edc42f18ce087f735c7c8bc9c8c614794b02ea
ILYA MALTSEV <i.y.maltsev@yandex.ru>
2017-02-07 16:38:53 +0400

    Update 4002_create_i_entities.rb

--- db/migrate/4002_create_i_entities.rb
+++ db/migrate/4002_create_i_entities.rb
@@ -1,5 +1,5 @@
 class CreateIEntities < ActiveRecord::Migration
-  def change
+  def up
     create_table :i_entities do |t|
       t.string :name, :limit => 64
       t.string :description, :limit => 128, :null => true
@@ -9,4 +9,9 @@ class CreateIEntities < ActiveRecord::Migration
       t.timestamps null: false
     end
   end
+
+  def down
+    drop_table :i_entities
+  end
 end
+
