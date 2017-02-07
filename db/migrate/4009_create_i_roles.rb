51fa0ff50fc11ad9b0c50921a7cfe85ab893a85d
ILYA MALTSEV <i.y.maltsev@yandex.ru>
2017-02-07 16:41:20 +0400

    Update 4009_create_i_roles.rb

--- db/migrate/4009_create_i_roles.rb
+++ db/migrate/4009_create_i_roles.rb
@@ -1,5 +1,5 @@
 class CreateIRoles < ActiveRecord::Migration
-  def change
+  def up
     create_table :i_roles do |t|
       t.belongs_to :i_resource, index: true
       t.belongs_to :updated_by, :null => true
@@ -8,5 +8,9 @@ class CreateIRoles < ActiveRecord::Migration
       t.boolean :deleted
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :i_roles
   end
 end
