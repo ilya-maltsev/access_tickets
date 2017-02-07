f5b5ac381e88495082a59dd975dd4321034a2e94
ILYA MALTSEV <i.y.maltsev@yandex.ru>
2017-02-07 16:42:02 +0400

    Update 4011_create_i_settings.rb

--- db/migrate/4011_create_i_settings.rb
+++ db/migrate/4011_create_i_settings.rb
@@ -1,5 +1,5 @@
 class CreateISettings < ActiveRecord::Migration
-  def change
+  def up
     create_table :i_settings do |t|
       t.string :param, :limit => 256
       t.string :value, :limit => 1024
@@ -7,5 +7,9 @@ class CreateISettings < ActiveRecord::Migration
       t.belongs_to :updated_by, :null => true
       t.timestamps null: false
     end
+  end
+
+  def down
+    drop_table :i_settings
   end
 end
